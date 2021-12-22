53049dd63c57380970f558f9f6bb5fefc18f1170
bernardopiane <bernardopiane@users.noreply.github.com>
2021-12-22 17:28:06 -0300

    added all queries

--- src/components/PinDetail.jsx
+++ src/components/PinDetail.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
+// Icons
+import { MdDownloadForOffline } from 'react-icons/md'
 
-function PinDetail() {
+import { client, urlFor } from '../client'
+import MasonryLayout from './MasonryLayout'
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data'
+
+
+function PinDetail({ user }) {
+
     return (
         <div>
+
             PinDetail
         </div>
     )
